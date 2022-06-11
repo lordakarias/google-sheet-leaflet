fd03b5a6f4787c6af8bfc64aabd6feb92706e584
Zekarias <zekariabi@gmail.com>
2022-06-11 19:10:18 +0300

    adjusted api

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1B8Ey2vAiqSyNre76Q9StnGoG9zayqd5mdbZV4Hfh9JE/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = '';
+var googleApiKey = 'AIzaSyAi7-ZcJZu2r3jCMRrlVnU_vAGICx6_wsQ';
